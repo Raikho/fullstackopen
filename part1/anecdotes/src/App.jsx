5c438d096ef0fd80fb6390bf83a91d4b0fdc78bc
Mateen <zahidx297@yahoo.com>
2023-10-25 22:56:59 -0400

    setup anecdotes exercise

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App'
+
+const App = () => {
+  // const [selected, setSelected] = useState(0);
+
+  return (
+    <>
+      Anecdotes
+    </>
+  )
+}
+
+export default App

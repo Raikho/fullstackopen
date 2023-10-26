873a55109aa41d8e63de46897c20661cfa70a018
Mateen <zahidx297@yahoo.com>
2023-10-26 02:49:25 -0400

    breakout Course component for course-info

--- part2/course-info/src/App.jsx
+++ part2/course-info/src/App.jsx
@@ -1,32 +1,5 @@
-const Header = ({ course }) => <h2>{course}</h2>
+import Course from './components/Course'
 
-const Total = ({ parts }) => (
-  <h4>
-    Total of {parts.reduce((prev, curr) => curr.exercises + prev, 0)} exercises
-  </h4>
-)
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part => 
-      (
-        <Part part={part} key={part.id} />
-      )
-    )}
-  </>
-
-const Course = (props) => {
-  const { name, parts } = props.course;
-  return (
-    <div>
-      <Header course={name}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
-    </div>
-  )
-}
 const App = () => {
   const courses = [
     {

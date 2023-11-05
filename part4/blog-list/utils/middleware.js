7b9fca037d29493198e4229b8027a8b5a7a946de
Mateen <zahidx297@yahoo.com>
2023-11-05 04:22:06 -0500

    implement middleware

---
+++ part4/blog-list/utils/middleware.js
@@ -0,0 +1,33 @@
+const logger = require('./utils/logger')
+const morgan = require('morgan')
+
+const requestLogger = morgan(function (tokens, req, res) {
+	const body = JSON.stringify(req.body)
+	return [
+		tokens.method(req, res),
+		tokens.url(req, res),
+		tokens.status(req, res),
+		tokens.res(req, res, 'content-length'), '-',
+		tokens['response-time'](req, res), 'ms',
+		body === '{}' ? null : body,
+	].join(' ')
+})
+
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (err, req, res, next) => {
+	logger.error(err.message)
+	if (err.name === 'CastError')
+		return res.status(400).send({ error: 'malformatted id' })
+	else if (err.name === 'ValidationError')
+		return res.status(400).json({ error: err.message })
+	next(err)
+}
+
+module.exports = {
+	requestLogger,
+	unknownEndpoint,
+	errorHandler
+}

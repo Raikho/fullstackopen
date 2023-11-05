2c25d3775c55e462851ecb7ab6f267482cac7ade
Mateen <zahidx297@yahoo.com>
2023-11-05 05:14:35 -0500

    implement blog list step 1 & 2

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -1,17 +1,25 @@
-const logger = require('./utils/logger')
-const morgan = require('morgan')
+const logger = require('./logger')
 
-const requestLogger = morgan(function (tokens, req, res) {
-	const body = JSON.stringify(req.body)
-	return [
-		tokens.method(req, res),
-		tokens.url(req, res),
-		tokens.status(req, res),
-		tokens.res(req, res, 'content-length'), '-',
-		tokens['response-time'](req, res), 'ms',
-		body === '{}' ? null : body,
-	].join(' ')
-})
+const requestLogger = (req, res, next) => {
+	logger.info('Method:', req.method)
+	logger.info('Path  :', req.path)
+	logger.info('Body  :', req.body)
+	logger.info('---')
+	next()
+}
+
+// const morgan = require('morgan')
+// const requestLogger = morgan(function (tokens, req, res) {
+// 	const body = JSON.stringify(req.body)
+// 	return [
+// 		tokens.method(req, res),
+// 		tokens.url(req, res),
+// 		tokens.status(req, res),
+// 		tokens.res(req, res, 'content-length'), '-',
+// 		tokens['response-time'](req, res), 'ms',
+// 		body === '{}' ? null : body,
+// 	].join(' ')
+// })
 
 const unknownEndpoint = (req, res) => {
 	res.status(404).send({ error: 'unknown endpoint' })
@@ -19,10 +27,12 @@ const unknownEndpoint = (req, res) => {
 
 const errorHandler = (err, req, res, next) => {
 	logger.error(err.message)
+
 	if (err.name === 'CastError')
 		return res.status(400).send({ error: 'malformatted id' })
 	else if (err.name === 'ValidationError')
 		return res.status(400).json({ error: err.message })
+
 	next(err)
 }
 

ca2f4f24ed87eda0715f9bdd81190862282ce247
Mateen <zahidx297@yahoo.com>
2023-11-05 16:52:16 -0500

    implement blog list unit tests step 2

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,29 +1,64 @@
 const listHelper = require('../utils/list_helper')
 
-const rand = num => Math.floor(Math.random() * num)
-
-const genBlog = ({ id, title, author, url, likes }) => {
-	return {
-		__id: id || `${rand(1000000)}`,
-		title: title || `Random Title #${rand(1000)}`,
-		author: author || `Random Author #${rand(1000)}`,
-		url: url || `Random URL ${rand(1000)}`,
-		likes: likes || rand(10),
-		__v: 0,
+const blogs = [
+	{
+		_id: '5a422a851b54a676234d17f7',
+		title: 'React patterns',
+		author: 'Michael Chan',
+		url: 'https://reactpatterns.com/',
+		likes: 7,
+		__v: 0
+	},
+	{
+		_id: '5a422aa71b54a676234d17f8',
+		title: 'Go To Statement Considered Harmful',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+		likes: 5,
+		__v: 0
+	},
+	{
+		_id: '5a422b3a1b54a676234d17f9',
+		title: 'Canonical string reduction',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+		likes: 12,
+		__v: 0
+	},
+	{
+		_id: '5a422b891b54a676234d17fa',
+		title: 'First class tests',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+		likes: 10,
+		__v: 0
+	},
+	{
+		_id: '5a422ba71b54a676234d17fb',
+		title: 'TDD harms architecture',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+		likes: 0,
+		__v: 0
+	},
+	{
+		_id: '5a422bc61b54a676234d17fc',
+		title: 'Type wars',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+		likes: 2,
+		__v: 0
 	}
-}
+]
+
 describe('total likes', () => {
 	test('of empty list is zero', () => {
 		expect(listHelper.totalLikes([])).toBe(0)
 	})
 	test('of one blog equals the likes of that', () => {
-		const blog = genBlog({ likes: 10 })
-		expect(listHelper.totalLikes([blog])).toBe(10)
+		expect(listHelper.totalLikes([blogs[0]])).toBe(7)
 	})
 	test('of multiple blogs is summed correctly', () => {
-		const blog1 = genBlog({ likes: 10 })
-		const blog2 = genBlog({ likes: 20 })
-		const blog3 = genBlog({ likes: 30 })
-		expect(listHelper.totalLikes([blog1, blog2, blog3])).toBe(60)
+		expect(listHelper.totalLikes(blogs)).toBe(36)
 	})
 })

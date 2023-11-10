175a28c43aa232b66db0b20352e962bfec564196
Mateen <zahidx297@yahoo.com>
2023-11-09 23:25:20 -0500

    fix more linting errors

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -5,28 +5,28 @@ let token = null
 const setToken = newToken => token = `Bearer ${newToken}`
 
 const create = async newObject => {
-  const config = { headers: { Authorization: token } }
-  const response = await axios.post(baseUrl, newObject, config)
-  return response.data
+	const config = { headers: { Authorization: token } }
+	const response = await axios.post(baseUrl, newObject, config)
+	return response.data
 }
 
 const update = async updatedObject => {
-  const config = { headers: { Authorization: token } }
-  const url = `${baseUrl}/${updatedObject.id}`
-  const response = await axios.put(url, updatedObject, config)
-  return response.data
+	const config = { headers: { Authorization: token } }
+	const url = `${baseUrl}/${updatedObject.id}`
+	const response = await axios.put(url, updatedObject, config)
+	return response.data
 }
 
 const remove = async id => {
-  const config = { headers: { Authorization: token } }
-  const url = `${baseUrl}/${id}`
-  const response = await axios.delete(url, config)
-  return response.data
+	const config = { headers: { Authorization: token } }
+	const url = `${baseUrl}/${id}`
+	const response = await axios.delete(url, config)
+	return response.data
 }
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+	const request = axios.get(baseUrl)
+	return request.then(response => response.data)
 }
 
 export default { setToken, getAll, create, update, remove }
--- part5/bloglist-frontend/src/services/storage.js
+++ part5/bloglist-frontend/src/services/storage.js
@@ -1,18 +1,18 @@
 const storage = () => {}
 
 const save = (tag, object) => {
-    window.localStorage.setItem(tag, JSON.stringify(object))
+	window.localStorage.setItem(tag, JSON.stringify(object))
 }
 
 const load = tag => {
-    const json = window.localStorage.getItem(tag)
-    if (!json) 
-        return null
-    return JSON.parse(json)
+	const json = window.localStorage.getItem(tag)
+	if (!json)
+		return null
+	return JSON.parse(json)
 }
 
 const remove = tag => {
-    window.localStorage.removeItem(tag)
+	window.localStorage.removeItem(tag)
 }
 
 export default { save, load, remove }

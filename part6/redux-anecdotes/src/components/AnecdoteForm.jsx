d6f001431024f9559e4a9da2677a5c13f87e865e
Mateen <zahidx297@yahoo.com>
2023-11-14 06:35:39 -0500

    implement 6.15 Anecdotes and the backend, step 2

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,14 +1,18 @@
 import { useDispatch } from 'react-redux'
-import { createAnecdote } from '../reducers/anecdoteReducer'
+import { appendAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification, removeNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const create = event => {
+  const create = async (event) => {
     event.preventDefault()
     const content = event.target.content.value
-    dispatch(createAnecdote(content))
+    
+    const newAnecdote = await anecdoteService.createNew(content)
+
+    dispatch(appendAnecdote(newAnecdote))
     
     dispatch(setNotification(`created new anecdote '${content}'`))
     setTimeout(() => dispatch(removeNotification()), 5000)
--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -7,7 +7,7 @@ const getAll = async () => {
   return res.data
 }
 
-const createNew = async content => {
+const createNew = async (content) => {
   const object = { content, votes: 0 }
   const res = await axios.post(baseUrl, object)
   return res.data

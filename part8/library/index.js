aae0e3f5794b8da353f1964f1579000e5ea1d654
Mateen <zahidx297@yahoo.com>
2023-12-07 02:53:05 -0500

    implement 8.5 Books by genre

--- part8/library/index.js
+++ part8/library/index.js
@@ -102,7 +102,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String genre: String): [Book!]!
     allAuthors: [Author]
   }
 `
@@ -112,11 +112,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (args.author) {
-        return books.filter(b => b.author === args.author)
-      }
+      authorFilter = args.author
+        ? book => book.author === args.author
+        : () => true
+      genreFilter = args.genre
+        ? book => book.genres.includes(args.genre)
+        : () => true
 
-      return books
+      return books.filter(authorFilter).filter(genreFilter)
     },
     allAuthors: () => authors,
   },

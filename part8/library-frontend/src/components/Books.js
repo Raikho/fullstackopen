3390b0764938a93bfdad4110d73ab80066c77978
Mateen <zahidx297@yahoo.com>
2023-12-07 06:41:19 -0500

    implement 8.9 Books view

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,5 +1,18 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`
+
 const Books = props => {
-  const books = []
+  const result = useQuery(ALL_BOOKS)
+  if (result.loading) return <div>is loading...</div>
 
   return (
     <div>
@@ -12,7 +25,7 @@ const Books = props => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map(a => (
+          {result.data.allBooks.map(a => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>

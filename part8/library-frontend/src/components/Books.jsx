cfba421434f10edb3d2f043c3e3f12ca20390912
Mateen <zahidx297@yahoo.com>
2023-12-14 18:24:19 -0500

    implement 8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = props => {
+  const [genres, setGenres] = useState(['a test'])
+  const [filter, setFilter] = useState('all')
   const result = useQuery(ALL_BOOKS)
+
+  useEffect(() => {
+    if (!result.data || !result.data.allBooks) return
+
+    const array = []
+    result.data.allBooks.forEach(b =>
+      b.genres.forEach(g => (array.includes(g) ? null : array.push(g)))
+    )
+    array.push('all')
+    setGenres(array)
+  }, [result])
+
+  const handleGenre = genre => {
+    setFilter(genre)
+  }
+
   if (result.loading) return <div>is loading...</div>
+
+  const filteredBooks =
+    filter === 'all'
+      ? result.data.allBooks
+      : result.data.allBooks.filter(b => b.genres.includes(filter))
 
   return (
     <div>
@@ -11,11 +35,11 @@ const Books = props => {
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>title</th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {result.data.allBooks.map(b => (
+          {filteredBooks.map(b => (
             <tr key={b.id}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
@@ -24,6 +48,16 @@ const Books = props => {
           ))}
         </tbody>
       </table>
+      <ul className='genre-button-list'>
+        {genres.map(g => (
+          <li
+            key={g}
+            className={`genre-button${filter === g ? ' active' : ''}`}
+          >
+            <button onClick={() => handleGenre(g)}>{g}</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }

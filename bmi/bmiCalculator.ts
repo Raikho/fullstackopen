775d1081ef3841c9614f084af4510f255e2e4b43
Mateen <zahidx297@yahoo.com>
2024-02-19 05:15:30 -0500

    add cli args to bmiCalculator

--- bmi/bmiCalculator.ts
+++ bmi/bmiCalculator.ts
@@ -1,3 +1,19 @@
+interface stats {
+  cm: number
+  kg: number
+}
+
+const parseArgs = (args: string[]): stats => {
+  if (args.length < 2) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  const cm: number = Number(args[2])
+  const kg: number = Number(args[3])
+  if (isNaN(cm) || isNaN(kg)) throw new Error('Arguments were not numbers!')
+
+  return { cm, kg }
+}
+
 const calculateBmi = (cm: number, kg: number): string => {
   const bmi: number = (kg / (cm * cm)) * 100 * 100
   // console.log('bmi calc: ', bmi) // debug
@@ -12,4 +28,13 @@ const calculateBmi = (cm: number, kg: number): string => {
   else return 'Obese (class III)'
 }
 
-console.log(calculateBmi(180, 74))
+// console.log(calculateBmi(180, 74))
+
+try {
+  const { cm, kg } = parseArgs(process.argv)
+  console.log(calculateBmi(cm, kg))
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) errorMessage += error.message
+  console.log(errorMessage)
+}
